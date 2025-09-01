bc2266d70b5df380b6cecbd531058cd57de801bf
l-fausti <lucasffarias.04@gmail.com>
2025-08-31 22:39:31 -0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public int numeroVaga;
     public static void main(String[] args) {
         System.out.println("Bem-vindo ao Estaciona Fácil");
         System.out.println("Veja as disponibilidades de nossas vagas");
